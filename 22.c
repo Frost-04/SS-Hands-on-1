e9ed242d5398f43eb94b22b96af7ef2726a8b992
Gaurav Verma <77579876+Frost-04@users.noreply.github.com>
2024-09-06 15:06:43 +0530

    22.c

--- 22.c
+++ 22.c
@@ -2,9 +2,9 @@
 ============================================================================
 Name : 22.c
 Author : Gaurav Verma
-Description : Write a program, open a file, call fork, and then write to the file by both the child as well as the
-              parent processes. Check output of the file. 
-Date: 24th Aug, 2024.
+Description : Write a program, open a file, call fork, and then write to the
+file by both the child as well as the parent processes. Check output of the
+file. Date: 24th Aug, 2024.
 ============================================================================
 */
 #include <fcntl.h>
@@ -12,22 +12,22 @@ Date: 24th Aug, 2024.
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if (fork() == 0) {
-        int fd = open(argv[1], O_WRONLY | O_APPEND | O_CREAT, 0700);
-        if (fd == -1) {
-            perror("Error!");
-            close(fd);
-            return 0;
-        }
-        char buffer[30];
-        snprintf(buffer, sizeof(buffer), "Process %d wrote this.\n", getpid());
-        write(fd, buffer, sizeof(buffer));
+    fork();
+    int fd = open(argv[1], O_WRONLY | O_APPEND | O_CREAT, 0700);
+    if (fd == -1) {
+        perror("Error!");
         close(fd);
+        return 0;
     }
+    char buffer[30];
+    snprintf(buffer, sizeof(buffer), "Process %d wrote this.\n", getpid());
+    write(fd, buffer, sizeof(buffer));
+    close(fd);
+
     return 0;
 }
 
+// Output Sample:-
 
-//Output Sample:-
-
-//Process 58 wrote this.
+// Process 58 wrote this.
+// Process 59 worte this.
